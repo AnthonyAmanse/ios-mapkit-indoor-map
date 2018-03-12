0efebd5a8de4951e032f17801bf5bb6b7ad9247e
AnthonyAmanse <ghieamanse@gmail.com>
2018-03-11 18:11:32 -0700

    remove unused script
    Signed-off-by: AnthonyAmanse <ghieamanse@gmail.com>

--- trigger-generator.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-i=0
-while [[ "$i" -lt 10 ]]; do
-zone=$(awk -v min=1 -v max=9 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
-echo $zone
-curl -H "Content-Type: application/json" -X POST -d '{"zone":"'"$zone"'","event":"enter"}' -s "http://heatmap-backend.mybluemix.net/triggers/add" -o /dev/null
-((i++))
-done
